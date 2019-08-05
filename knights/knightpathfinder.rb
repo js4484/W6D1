8b7ebbc43cf0f9f6fdd611ac3555c8cbed2ea791
js4484 <js4484@columbia.edu>
2019-08-05 18:09:38 -0400

    test for aliashost

--- knights/knightpathfinder.rb
+++ knights/knightpathfinder.rb
@@ -70,3 +70,6 @@ end
 kpf = KnightPathFinder.new([0, 0])
 p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
 p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
+
+
+# alias gitguest="git config --local user.email guest_email@aol.com && git config --local user.name Guest_Name"
